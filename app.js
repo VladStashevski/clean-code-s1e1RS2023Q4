139dcd777b36a9ff4f2b2c544d4b6097f721b8cf
VladSt <adrenaline2142@gmail.com>
2024-01-11 22:15:27 +0500

    feat: add new createNewTaskElement deleteButtonImg.alt="remove button"

--- app.js
+++ app.js
@@ -50,6 +50,7 @@ var createNewTaskElement  =  function(taskString){
 
     deleteButton.className = "btn btn-delete";
     deleteButtonImg.src = './remove.svg';
+    deleteButtonImg.alt="remove button";
     deleteButtonImg.className = "delete-img";
     deleteButton.appendChild(deleteButtonImg);
 
